26b16d2198a5ea777df9c55af697553987979383
skverma2021 <s.k.verma@live.in>
2025-07-03 05:20:53 +0530

    mod1

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,7 @@
 // server/db.js
 module.exports = {
   user: process.env.DB_USER || 'your_user',
-  // password: process.env.DB_PASSWORD || 'your_password',
-  password: process.env.DB_PASSWORD || 'theUdemyUser',
+  password: process.env.DB_PASSWORD || 'your_password',
   server: process.env.DB_SERVER || 'localhost',
   database: process.env.DB_DATABASE || 'compliance_db',
   options: {
